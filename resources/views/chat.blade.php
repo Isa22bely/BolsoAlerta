4309901f1137e8b30fb1ca49368da9ebf58d165e
Isa22bely <isabelyvga22@gmail.com>
2024-10-24 10:38:02 -0300

    ajustes

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -13,44 +13,53 @@
 <div class="entrecard">
     
         <div class="ard text-center" style="max-width: 80%;">
-        <div class="cardechat" >
-            <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                <img src="{{asset('storage/exemplo/mensagen.svg')}}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                    Maria das Graças de Souza
-                </a>
-            </div>
-            </nav>
-            <div class="bloco">
-                <div class="blocoD">
-                    <div class="card" style="max-width: 50%;" >
-                        <p class="titulo">Corpo de Bombeiros</p>
+            <div class="cardechat" >
+                <nav class="navbar bg-body-tertiary"  >
+                    <div class="container-fluid" >
+                        <a class="navbar-brand" href="#">
+                        <img src="{{asset('storage/exemplo/mensagen.svg')}}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top" >
+                            Maria das Graças de Souza
+                        </a>
+                    </div>
+                </nav>
+                <div class="bloco">
+                    <div class="blocoD">
+                        <div class="card" style="max-width: 50%;" >
+                            <p class="titulo">Corpo de Bombeiros</p>
+                                <div class="card-body">
+                                    <p class="text-break" >
+                                    Há alguém preso na residência?
+                                    </p>
+                                </div>
+                        </div>
+                    </div>
+
+                    <div class="blocoE">
+                        <div class="card" style="max-width: 50%;" >
+                            <p class="titulo">Maria das Graças</p>
+                            <div>
+                                <div class="text-center" >
+                                    <img src="{{asset('storage/exemplo/image.png')}}" class="img-fluid"  alt="..." styles=" margin: 10%">
+                                </div>
+                            </div>
                             <div class="card-body">
                                 <p class="text-break" >
-                                Há alguém preso na residência?
+                                    Tem 2 pessoas lá dentro
                                 </p>
                             </div>
-                    </div>
-                </div>
-                <div class="blocoE">
-                    <div class="card" style="max-width: 50%;" >
-                        <p class="titulo">Maria das Graças</p>
-                        <div>
-                            <div class="text-center" >
-                                <img src="{{asset('storage/exemplo/image.png')}}" class="img-fluid"  alt="...">
-                            </div>
-                        </div>
-                        <div class="card-body">
-                            <p class="text-break" >
-                                Tem 2 pessoas lá dentro
-                            </p>
                         </div>
                     </div>
-                </div>
+                    <div >
+                        <div class="envmensagen" >
+                            <input  type="mensagen" class="rolagem" id="examplemensagem" > 
+
+                            
+                        </div>  
+                    </div>  
+                    
             </div>
         </div>
-    </div>
+       
 </div>
 
 
